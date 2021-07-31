f306ed4570220b7f8a21d4b7c0a147adf28d74df
Enson-Mike <ensonrodrigues26@gmail.com>
2021-08-01 01:46:41 +0530

    Updated the React code
    Updated the React code

---
+++ sample-03-react/src/AppContainer.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import {
+    Route,
+    NavLink,
+    HashRouter
+} from "react-router-dom";
+
+// Menu pages
+import Home from "./component/Home";
+import About from "./component/About";
+import Contact from "./component/Contact";
+import Article from "./component/Article"
+
+import './AppContainer.css';
+
+//<Route exact path="/" component={props => <Home {...props} />}/> 
+
+function AppContainer() {
+  return (
+    <HashRouter>
+          <>
+            <ul className="header">
+              <li><img src="https://assets.bupa.co.uk/Assets/Global/Components/css/img/icon/logo.png"/></li>
+              <li><NavLink to="/">Home</NavLink></li>
+              <li><NavLink to="/article">Our Article</NavLink></li>
+              <li><NavLink to="/about">About</NavLink></li>
+              <li><NavLink to="/contact">Contact</NavLink></li>
+            </ul>
+            <div className="content">
+              <Route exact path="/" component={Home}/> 
+              <Route path="/about" component={About}/>
+              <Route path="/contact" component={Contact} />
+              <Route path="/article" component={Article} />
+            </div>
+            <ul className="footer">
+              <li><NavLink to="/">Home</NavLink></li>
+              <li><NavLink to="/article">Our Article</NavLink></li>
+              <li><NavLink to="/about">About</NavLink></li>
+              <li><NavLink to="/contact">Contact</NavLink></li>
+            </ul>
+          </>
+        </HashRouter>
+  );
+}
+
+export default AppContainer;
