60bfeb1ce13ab4e597021c8844275d83d56f130e
Enson-Mike <ensonrodrigues26@gmail.com>
2022-04-09 11:22:12 +0530

    added Contact js

---
+++ sample-08-test01/src/pages/ContactUs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ContactUs = () => {
+    return (
+        <section className="layer1">
+            <div className="vessel">
+                <p className="h2">ContactUs Section</p>
+            </div>
+        </section>
+    )
+}
+
+export default ContactUs;
--- sample-08-test01/src/router/config.js
+++ sample-08-test01/src/router/config.js
@@ -4,6 +4,7 @@ import { lazy } from "react";
 const Home = lazy(()=> import('../pages/Home'));
 const OurTeam = lazy(()=> import('../pages/OurTeam'));
 const OurLocation = lazy(()=> import('../pages/OurLocation'));
+const ContactUs = lazy(()=> import('../pages/ContactUs'))
 //import Home from "../pages/Home";
 //import OurTeam from "../pages/OurTeam";
 //import OurLocation from "../pages/OurLocation";
@@ -26,6 +27,12 @@ const jsonData = [
       "component": OurLocation,
       "name": "Our Location",
       "data": "Map"
+    },
+    {
+      "path": "/contactus",
+      "component": ContactUs,
+      "name": "Contact Us",
+      "data": ""
     }
   ]
 
